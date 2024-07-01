ce2492c1ad7da4f671a94ddf0dfe0a1e884c8d7a
AmeyAvhad <75404011+AmeyAvhad@users.noreply.github.com>
2024-07-01 09:43:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ NUM_WORKERS = 4 # Number of parallel workers for data loading.
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 # Training images and XML files directory.
-TRAIN_DIR = '/content/Retinanet-1/train'
+TRAIN_DIR = '/content/kao-1/train'
 # Validation images and XML files directory.
-VALID_DIR = '/content/Retinanet-1/valid'
+VALID_DIR = '/content/kao-1/valid'
 
 # Classes: 0 index is reserved for background.
 CLASSES = [
@@ -20,7 +20,7 @@ CLASSES = [
 NUM_CLASSES = len(CLASSES)
 
 # Whether to visualize images after crearing the data loaders.
-VISUALIZE_TRANSFORMED_IMAGES = True
+VISUALIZE_TRANSFORMED_IMAGES = False
 
 # Location to save model and plots.
 OUT_DIR = 'outputs'
